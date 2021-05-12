d47af020279a5fcc04ee66d6db3df18d3fe2fdaa
amolow <aliolow7@gmail.com>
2021-05-12 11:44:29 +0700

    fix error

--- class/class.Project.php
+++ class/class.Project.php
@@ -2,49 +2,53 @@
 include 'class.Department.php';
 class Project extends Connection
 {
-    private $pnumber='';
-    private $pname = '';
-    private $plocation = '';
-    private $dept;
-    private $hasil = false;
-    private $message ='';
-    public function __get($atribute) {
-        if (property_exists($this, $atribute)) {
-            return $this->$atribute;
-        }
-    }
-    public function __set($atribut, $value){
-        if (property_exists($this, $atribut)) {
-            
-        }
-    }
-    function __construct() {
-        parent::__construct();
-        $this->dept = new Department();
-    }
+private $pnumber='';
+private $pname = '';
+private $plocation = '';
+private $dept;
+private $hasil = false;
+private $message ='';
+public function __get($atribute) {
+if (property_exists($this, $atribute)) {
+return $this->$atribute;
+}
+}
+public function __set($atribut, $value){
+if (property_exists($this, $atribut)) {
+$this->$atribut = $value;
+}
+}
+function __construct() {
+parent::__construct();
+$this->dept = new Department();
+}
 
-    public function SelectAllProject(){
-        $sql = "SELECT p.*, d.dname
-        FROM project p INNER JOIN department d
-        ON p.dnum = d.dnumber
-        ORDER BY p.pnumber";
-        $result = mysqli_query($this->connection, $sql);
-        $arrResult = Array();
-        $cnt=0;
-        if(mysqli_num_rows($result) > 0){
-            while ($data = mysqli_fetch_array($result)){
-                $objProject = new Project();
-                $objProject->pnumber=$data['pnumber'];
-                $objProject->pname=$data['pname'];
-                $objProject->plocation=$data['plocation'];
-                $objProject->dept->dnumber =$data['dnum'];
-                $objProject->dept->dname =$data['dname'];
-                $arrResult[$cnt] = $objProject;
-                $cnt++;
-            }
-        }
-        return $arrResult;
+
+public function SelectAllProject(){
+    $sql = "SELECT p.*, d.dname
+    FROM project p INNER JOIN department d
+    ON p.dnum = d.dnumber
+    ORDER BY p.pnumber";
+    $result = mysqli_query($this->connection, $sql);
+    $arrResult = Array();
+    $cnt=0;
+    if(mysqli_num_rows($result) > 0){
+    while ($data = mysqli_fetch_array($result)){
+    $objProject = new Project();
+    $objProject->pnumber=$data['pnumber'];
+    $objProject->pname=$data['pname'];
+    $objProject->plocation=$data['plocation'];
+    $objProject->dept->dnumber =$data['dnum'];
+    $objProject->dept->dname =$data['dname'];
+    $arrResult[$cnt] = $objProject;
+    $cnt++;
     }
+    }
+    return $arrResult;
+    }
+    
+
+    
     public function AddProject(){
         $sql = "INSERT INTO project (pnumber, pname, plocation, dnum)
         VALUES ($this->pnumber, '$this->pname', '$this->plocation', ".$this->dept->dnumber.")";
@@ -53,8 +57,8 @@ class Project extends Connection
         $this->message ='Data berhasil ditambahkan!';
         else
         $this->message ='Data gagal ditambahkan!';
-    }
-    public function UpdateProject(){
+        }
+        public function UpdateProject(){
         $sql = "UPDATE project
         SET pname ='$this->pname',
         plocation = '$this->plocation',
@@ -65,30 +69,6 @@ class Project extends Connection
         $this->message ='Data berhasil diubah!';
         else
         $this->message ='Data gagal diubah!';
-    }
-
-
-    public function SelectOneProject(){
-        $sql = "SELECT * FROM project WHERE pnumber=$this->pnumber";
-        $resultOne = mysqli_query($this->connection, $sql) or die(mysqli_error($this->connection));
-        if(mysqli_num_rows($resultOne) == 1){
-            $this->hasil = true;
-            $data = mysqli_fetch_assoc($resultOne);
-            $this->pnumber=$data['pnumber'];
-            $this->pname=$data['pname'];
-            $this->plocation=$data['plocation'];
-            $this->dept->dnumber =$data['dnum'];
         }
+
     }
-    public function DeleteProject(){
-        $sql = "DELETE FROM project WHERE pnumber=$this->pnumber";
-        $this->hasil = mysqli_query($this->connection, $sql);
-        if($this->hasil)
-        $this->message ='Data berhasil dihapus!';
-        else
-        $this->message ='Data gagal dihapus!';
-    }
-
-
-
-}
