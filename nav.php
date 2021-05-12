1e2e29431af167bc32c6687a7f68b5f952bf4d5c
amolow <aliolow7@gmail.com>
2021-05-12 11:40:17 +0700

    commit files

---
+++ nav.php
@@ -0,0 +1,39 @@
+    <!-- NAVBAR -->
+    <div class="hello solid">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="container-fluid">
+        <img src="Asset/esq.png" alt="" />
+        <div class="logo" style="margin-left: 2em">
+          <h2>Company165</h2>
+          <p>
+          Database <br />
+            
+          </p>
+        </div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav" style="margin-left: 5em">
+            <a class="nav-link text-white" href="index.php?p=employeelist">Employee List</a>
+            <a class="nav-link text-white" href="index.php?p=employee">Employee</a>
+            <a class="nav-link text-white" href="index.php?p=projectlist">Project List</a>
+            <a class="nav-link text-white" href="index.php?p=project">Project</a>
+
+          </div>
+        </div>
+        <a href="pages/login.php">
+          <img src="Asset/logout.png" alt="" id="logout" />
+      </a>
+      </div>
+    </nav>
+    <!-- PENUTUP NAVBAR -->
+  </div>
---
+++ pages/home.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1>Home</h1>
+    <a href="index.php?p=upload">upload</a>
+    <!-- <a href="../upload/"></a> -->
+</div>
